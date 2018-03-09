480827b1b346c695b6058c7364975c78034f304e
circleCI <circleci@burib.net>
2018-03-09 15:12:14 +0100

    feat(infra): test deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+git config --global user.email 'circleci@burib.net';
+git config --global user.name 'circleCI';
+
+if [ -n "$(git status --porcelain)" ]; then
+  	git add -A;
+	timestamp=$(date \"+%F_%T\");
+	git commit -m "$timestamp";
+	npm version patch;
+	git push --force; 
+
+else
+  echo "no changes";
+fi
