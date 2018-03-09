6f8b580c5931dcb89b871149a0b4a1ade8d631bd
Balázs Buri <burib@users.noreply.github.com>
2018-03-09 16:07:51 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,16 +6,14 @@ git config user.email 'circleci@burib.net';
 git config user.name 'circleCI';
 git pull $CIRCLE_REPOSITORY_URL $CIRCLE_BRANCH;
 
-cd ./data/;
-
-if git diff-index --name-status --exit-code HEAD;
+if [[ -z $(git status -s) ]]
 then
-    echo "no changes";
+  echo "no changes";
 else
     echo "changes detected.";
-  	git add -A;
-	timestamp=$(date --utc "+%F_%T");
-	git commit -m "$timestamp";
-	npm version patch;
-	git push $CIRCLE_REPOSITORY_URL $CIRCLE_BRANCH --force; 
+    git add -A;
+    timestamp=$(date --utc "+%F_%T");
+    git commit -m "$timestamp";
+    npm version patch;
+    git push $CIRCLE_REPOSITORY_URL $CIRCLE_BRANCH --force;
 fi
