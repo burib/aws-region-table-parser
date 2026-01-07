59d6ff38a6eceb747888968c671ad5d43e4e2bae
burib <burib@users.noreply.github.com>
2026-01-07 14:20:17 +0100

    feat: Add height parameter to getChartUrl function

--- chart-generator.js
+++ chart-generator.js
@@ -1,17 +1,20 @@
 const request = require('request');
 
 module.exports = {
-  getChartUrl: (chartConfig = {}) => {
+  getChartUrl: (chartConfig = {}, height = 600) => { // Added height parameter
     return new Promise((resolve, reject) => {
       request.post('https://quickchart.io/chart/create', {
-        json: { "chart": chartConfig }
-      }, (error, res, body) => {
-        if (error) {
-          reject(error);
+        json: { 
+            "chart": chartConfig,
+            "width": 600,
+            "height": height
         }
-
+      }, (error, res, body) => {
+        if (error || !body.url) {
+          reject(error || new Error('Failed to get URL'));
+        }
         resolve(body.url);
       })
     })
   }
-};
+}
